19ea020182b7b8cf752474476be8c8a2a0f99b8c
tslever <46533664+tslever@users.noreply.github.com>
2022-08-27 23:04:41 -0400

    Transferred Convolve Audio
    Going to transfer solutions from other repositories

---
+++ Convolve_Audio/Convolve_Audio/ConvertIntDataToFloatData.cu
@@ -0,0 +1,12 @@
+﻿#ifndef STD_INT
+#define STD_INT
+#include <stdint.h>
+#endif
+
+__global__
+void ConvertIntDataToFloatData(float* floatDataOfAudioFile, int16_t* intDataOfAudioFile)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    floatDataOfAudioFile[index] = (float)intDataOfAudioFile[index];
+}
---
+++ Convolve_Audio/Convolve_Audio/PerformAugmentedConvolution.h
@@ -0,0 +1,3 @@
+__global__
+void PerformAugmentedConvolution(
+    float* convolvedHeyHoToTheGreenwood_child, float* floatDataOfHeyHoToTheGreenwood_child, float* floatDataOfImpulseResponse_child, long lengthOfFloatDataOfImpulseResponse_child);
