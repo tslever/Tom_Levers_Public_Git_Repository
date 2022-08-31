40519dd48b802efedf289809ecf542e6eca8532b
tslever <46533664+tslever@users.noreply.github.com>
2022-08-31 02:07:29 -0400

    Created a CSV file reader
    Going to use a CSV file reader

---
+++ tomleverspythonpackage/tomleverspythonpackage/ACsvFileReader.py
@@ -0,0 +1,18 @@
+class a_csv_file_reader:
+    
+    def __init__(self) -> None:
+        pass
+
+    def reads_into_a_dictionary(self, path: str) -> dict:
+        the_dictionary = {
+                
+        }
+        
+        with open(path, mode='r') as the_csv_file:
+            for line in the_csv_file:
+                the_stripped_line = line.rstrip('\n')
+                the_list_of_the_key_and_the_value = the_stripped_line.split(',')
+                the_key = the_list_of_the_key_and_the_value[0]
+                the_value = the_list_of_the_key_and_the_value[1]
+                the_dictionary[the_key] = the_value
+            return the_dictionary
