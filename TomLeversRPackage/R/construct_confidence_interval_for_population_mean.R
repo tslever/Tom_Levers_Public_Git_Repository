ca35d770104c6e5171917ff3b37307ad32ce1b37
tslever <46533664+tslever@users.noreply.github.com>
2022-09-11 13:58:51 -0400

    Refactored into construct_confidence_interval_for_population_mean
    Going to refactor other functions in TomLeversRPackage

--- TomLeversRPackage/R/construct_confidence_interval_for_population_mean.R
+++ TomLeversRPackage/R/construct_confidence_interval_for_population_mean.R
@@ -1,12 +1,12 @@
-#' @title constructConfidenceIntervalForPopulationMean
+#' @title construct_confidence_interval_for_population_mean
 #' @description Constructs a confidence interval for a population mean based on a set of statistics for a sample
 #' @param data_for_sample A vector of data for a sample
 #' @param significance_level A significance level (e.g., 0.05)
 #' @return A confidence interval
-#' @examples confidence_interval <- constructConfidenceIntervalForPopulationMean(iris$Sepal.Length, 0.05)
+#' @examples confidence_interval <- construct_confidence_interval_for_population_mean(iris$Sepal.Length, 0.05)
 #' @export
 
-constructConfidenceIntervalForPopulationMean <- function(data_for_sample, significance_level) {
+construct_confidence_interval_for_population_mean <- function(data_for_sample, significance_level) {
     number_of_data <- length(data_for_sample)
     degrees_of_freedom <- number_of_data - 1
     mean_value <- mean(data_for_sample)
