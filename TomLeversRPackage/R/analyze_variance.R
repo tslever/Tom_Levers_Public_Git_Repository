bcee112c774e957e87f7a91b19dd1677a4a5e1ce
tslever <46533664+tslever@users.noreply.github.com>
2022-09-11 13:39:41 -0400

    Added style information to README.md and refactored analyzeVariance to analyze_variance
    Going to refactor other functions in TomLeversRPackage

---
+++ TomLeversRPackage/R/analyze_variance.R
@@ -0,0 +1,45 @@
+#' @title analyze_variance
+#' @description Analyzes the variance of a linear model
+#' @param linear_model The linear model to analyze
+#' @return The analysis of variance for the linear model
+#' @examples analysis_of_variance <- analyze_variance(lm(iris$Sepal.Length ~ iris$Sepal.Width, data = iris))
+#' @import stringr
+#' @export
+
+analyze_variance <- function(linear_model) {
+    analysis <- capture.output(anova(linear_model))
+    regular_expression_for_decimal <- "-?\\d+(\\.\\d+)?"
+
+    line_with_SSR <- analysis[5]
+    DF_SSR_MSR_F0_and_P <- str_extract_all(line_with_SSR, regular_expression_for_decimal)[[1]]
+    SSR <- as.double(DF_SSR_MSR_F0_and_P[2])
+    line_with_SSRes <- analysis[6]
+    DF_SSRes_and_MSRes <- str_extract_all(line_with_SSRes, regular_expression_for_decimal)[[1]]
+    SSRes <- as.double(DF_SSRes_and_MSRes[2])
+    SST <- SSR + SSRes
+    line_with_SST <- paste("SST: ", SST, sep = "")
+    analysis <- append(analysis, line_with_SST)
+
+    coefficient_of_determination_R2 <- SSR / SST
+    line_with_R2 <- paste("R2: ", coefficient_of_determination_R2, sep = "")
+    analysis <- append(analysis, line_with_R2)
+
+    number_of_observations <- nobs(linear_model)
+    line_with_number_of_observations <- paste("Number of observations: ", number_of_observations, sep = "")
+    analysis <- append(analysis, line_with_number_of_observations)
+
+    analysis <- paste(analysis, collapse = "\n")
+    class(analysis) <- "analysis_of_variance"
+    return(analysis)
+}
+
+
+#' @title print.analysis_of_variance
+#' @description Adds to the generic function print the analysis_of_variance method print.analysis_of_variance
+#' @param analysis The analysis to print
+#' @examples print(analysis_of_variance)
+#' @export
+
+print.analysis_of_variance <- function(analysis) {
+    cat(analysis)
+}
--- TomLeversRPackage/README.md
+++ TomLeversRPackage/README.md
@@ -4,7 +4,20 @@ Contains Tom Lever's R functions
 To rebuild this package, open the TomLeversRPackage project in RStudio and run devtools::document() in the RStudio console.
 
 To install this package, run in the RStudio console
-install.packages("C:\\Users\\Tom\\Documents\\Tom_Levers_Git_Repository\\TomLeversRPackage", repo = NULL, type="source")
+install.packages("~\\Documents\\Tom_Levers_Git_Repository\\TomLeversRPackage", repo = NULL, type="source")
 
 Regarding creating this package
 https://www.r-bloggers.com/2020/07/how-to-write-your-own-r-package-and-publish-it-on-cran/
+
+Per "Writing R Extensions" at https://cran.r-project.org/doc/manuals/R-exts.html#The-DESCRIPTION-file,
+"The mandatory ‘Package’ field gives the name of the package. This should contain only (ASCII) letters, numbers and dot, have at least two characters and start with a letter and not end in a dot."
+
+Per "Files" at https://style.tidyverse.org/files.html,
+"Avoid using special characters in file names - stick with numbers, letters, -, and _.
+# Good
+fit_models.R"
+
+Per "Syntax",
+"Variable and function names should use only lowercase letters, numbers, and _. Use underscores (_) (so called snake case) to separate words within a name.
+# Good
+day_one"
