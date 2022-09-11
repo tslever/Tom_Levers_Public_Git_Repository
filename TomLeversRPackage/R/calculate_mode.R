6c6b9079f7caa27888a6b310d7bed37364d0732d
tslever <46533664+tslever@users.noreply.github.com>
2022-09-11 13:46:27 -0400

    Continued to refactor analyzeVariance into analyze_variance, and refactored calculateMode into calculate_mode
    Going to refactor other functions in TomLeversRPackage

--- TomLeversRPackage/R/calculate_mode.R
+++ TomLeversRPackage/R/calculate_mode.R
@@ -1,11 +1,11 @@
-#' @title calculateMode
+#' @title calculate_mode
 #' @description Calculates the mode given data for a sample
 #' @param data_for_sample A vector of data for a sample
 #' @return The mode
-#' @examples mode <- calculateMode(iris$Sepal.Length)
+#' @examples mode <- calculate_mode(iris$Sepal.Length)
 #' @export
 
-calculateMode <- function(data_for_sample) {
+calculate_mode <- function(data_for_sample) {
     unique_values <- unique(data_for_sample)
     positions_of_first_matches = match(data_for_sample, unique_values)
     frequencies_of_positions <- tabulate(positions_of_first_matches)
