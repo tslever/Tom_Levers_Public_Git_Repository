5af6509d65a5ab2b17d478da225b653503888492
tslever <46533664+tslever@users.noreply.github.com>
2022-09-16 00:43:20 -0400

    Removed print.hypothesis_test_result from test_null_hypothesis_involving_proportion
    Going to investigate TomLeversRBox

--- TomLeversRPackage/R/test_null_hypothesis_involving_proportion.R
+++ TomLeversRPackage/R/test_null_hypothesis_involving_proportion.R
@@ -7,8 +7,8 @@
 #' @param significance_level A significance level
 #' @return test_result A message rejecting or not rejecting the null hypothesis, and supporting or not supporting the alternate hypothesis
 #' @examples test_result <- test_null_hypothesis_involving_proportion(0.758, 0.505, 467, ">=", 0.05)
-
 #' @export
+
 test_null_hypothesis_involving_proportion <- function(population_proportion, sample_proportion, number_of_data_for_sample, comparator, significance_level) {
     test_statistic_z <- (sample_proportion - population_proportion) / sqrt(population_proportion * (1 - population_proportion) / number_of_data_for_sample)
     if (comparator == "=") {
@@ -21,14 +21,4 @@ test_null_hypothesis_involving_proportion <- function(population_proportion, sam
         stop("You provided an invalid comparator.")
     }
     return(generate_hypothesis_test_result(probability, significance_level))
-}
-
-#' @title print.hypothesis_test_result
-#' @description Adds to the generic function print the hypothesis_test_result method print.hypothesis_test_result
-#' @param the_summary The hypothesis-test result to print
-#' @examples print(hypothesis_test_result)
-#' @export
-
-print.hypothesis_test_result <- function(hypothesis_test_result) {
-    cat(hypothesis_test_result)
 }
