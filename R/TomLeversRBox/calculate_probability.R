1c710b6694be51171ce77a76dce1128751f6fe71
Tom Lever <tlever@Toms-MacBook-Pro.local>
2022-09-28 15:20:30 -0400

    Using canonical box, allowed registering S3 method print.message with generic method print. Added example function documentation. Added option to export nothing and disable attaching.

--- R/TomLeversRBox/calculate_probability.R
+++ R/TomLeversRBox/calculate_probability.R
@@ -1,7 +1,7 @@
-box::use(./print.message[print.message])
-box::register_S3_method('print', 'message', print.message)
 box::use(stats[pnorm])
 
+#' @title calculate_probability
+#' @description Calculates the probability of a test statistic being less than a specific quantile of a standard normal distribution
 #' @export
 calculate_probability <- function() {
     message <- paste("Probability: ", round(pnorm(1.644854, 0, 1, lower.tail = TRUE), 2), sep = "")
--- R/TomLeversRBox/print.message.R
+++ R/TomLeversRBox/print.message.R
@@ -1,3 +1,6 @@
+#box::export() # Use to export nothing and disable attaching (https://klmr.me/box/reference/export.html)
 print.message <- function(message) {
     cat(message)
 }
+
+box::register_S3_method('print', 'message', print.message)
