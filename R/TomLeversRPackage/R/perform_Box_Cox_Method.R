39c24559754c7bee3a3fd2a9f74db201867a76bd
tslever <46533664+tslever@users.noreply.github.com>
2022-10-01 14:35:32 -0400

    Changed path to TomLeversRPackage in R command. perform_Box_Cox_Method provides transformed predictor values. Completed homework for Model Diagnostics and Remedial Measures in SLR.
    Going to start first project for Linear Models

--- R/TomLeversRPackage/R/perform_Box_Cox_Method.R
+++ R/TomLeversRPackage/R/perform_Box_Cox_Method.R
@@ -5,6 +5,7 @@
 #' @param whether_to_plot Defaults to true
 #' @return result_of_Box_Cox_Method
 #' @examples result_of_Box_Cox_Method <- perform_Box_Cox_Method(linear_model)
+#' @import MASS
 
 #' @export
 perform_Box_Cox_Method <- function(linear_model, vector_of_values_of_lambda = seq(-2, 2, 0.1), whether_to_plot = TRUE) {
@@ -14,26 +15,34 @@ perform_Box_Cox_Method <- function(linear_model, vector_of_values_of_lambda = se
     maximum_likelihood <- max(likelihoods)
     index_of_maximum_likelihood_in_likelihoods <- match(maximum_likelihood, likelihoods)
     parameters <- Box_Cox_plot_data$x
-    the_maximum_likelihood_estimate_of_parameter_lambda <- parameters[index_of_maximum_likelihood_in_likelihoods]
+    maximum_likelihood_estimate_of_parameter_lambda <- parameters[index_of_maximum_likelihood_in_likelihoods]
 
+    predictor_values <- unlist(linear_model$model[2])
+    the_transformed_predictor_values <- Box_Cox_equation(predictor_values, maximum_likelihood_estimate_of_parameter_lambda)
     fitted_values <- linear_model$fitted.values
-    logarithmicized_fitted_values <- log(fitted_values)
-    sum_of_logarithmicized_fitted_values <- sum(logarithmicized_fitted_values)
-    average_of_logarithmicized_fitted_values <- sum_of_logarithmicized_fitted_values / length(logarithmicized_fitted_values)
-    log_of_average_of_logarithmicized_fitted_values <- log(average_of_logarithmicized_fitted_values)
-    reciprocal_of_log_of_average_of_logarithmicized_fitted_values <- 1 / log_of_average_of_logarithmicized_fitted_values
-
-    the_transformed_fitted_values <- numeric(length(fitted_values))
-    if (the_maximum_likelihood_estimate_of_parameter_lambda == 0) {
-        the_transformed_fitted_values <- reciprocal_of_log_of_average_of_logarithmicized_fitted_values * log(fitted_values)
-    } else {
-        the_transformed_fitted_values <- (fitted_values^the_maximum_likelihood_estimate_of_parameter_lambda - 1) / (the_maximum_likelihood_estimate_of_parameter_lambda * reciprocal_of_log_of_average_of_logarithmicized_fitted_values^(the_maximum_likelihood_estimate_of_parameter_lambda - 1))
-    }
+    the_transformed_fitted_values <- Box_Cox_equation(fitted_values, maximum_likelihood_estimate_of_parameter_lambda)
 
     result_of_Box_Cox_Method <- list(
-        maximum_likelihood_estimate_of_parameter_lambda = the_maximum_likelihood_estimate_of_parameter_lambda,
+        maximum_likelihood_estimate_of_parameter_lambda = maximum_likelihood_estimate_of_parameter_lambda,
+        transformed_predictor_values = the_transformed_predictor_values,
         transformed_fitted_values = the_transformed_fitted_values
     )
     class(result_of_Box_Cox_Method) <- "result_of_Box_Cox_Method"
     return(result_of_Box_Cox_Method)
+}
+
+Box_Cox_equation <- function(values, maximum_likelihood_estimate_of_parameter_lambda) {
+    logarithmicized_values <- log(values)
+    sum_of_logarithmicized_values <- sum(logarithmicized_values)
+    average_of_logarithmicized_values <- sum_of_logarithmicized_values / length(logarithmicized_values)
+    log_of_average_of_logarithmicized_values <- log(average_of_logarithmicized_values)
+    reciprocal_of_log_of_average_of_logarithmicized_values <- 1 / log_of_average_of_logarithmicized_values
+
+    transformed_values <- numeric(length(values))
+    if (maximum_likelihood_estimate_of_parameter_lambda == 0) {
+        transformed_values <- reciprocal_of_log_of_average_of_logarithmicized_values * log(values)
+    } else {
+        transformed_values <- (values^maximum_likelihood_estimate_of_parameter_lambda - 1) / (maximum_likelihood_estimate_of_parameter_lambda * reciprocal_of_log_of_average_of_logarithmicized_values^(maximum_likelihood_estimate_of_parameter_lambda - 1))
+    }
+    return(transformed_values)
 }
