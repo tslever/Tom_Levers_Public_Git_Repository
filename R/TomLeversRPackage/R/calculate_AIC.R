89f7727959d55a20826fdda60ad9941e73b28466
Tom Lever <thomas.lever.business@gmail.com>
2023-06-01 11:56:40 -0600

    Allowed calculate_AIC to return a scalar

--- R/TomLeversRPackage/R/calculate_AIC.R
+++ R/TomLeversRPackage/R/calculate_AIC.R
@@ -4,4 +4,5 @@ calculate_AIC <- function(linear_model) {
     residual_sum_of_squares <- calculate_residual_sum_of_squares(linear_model)
     number_of_variables <- get_number_of_variables(linear_model)
     Akaike_Information_Criterion <- number_of_observations * log(residual_sum_of_squares / number_of_observations, base = exp(1)) + 2 * number_of_variables
+    return(Akaike_Information_Criterion)
 }
--- R/TomLeversRPackage/R/calculate_residual_sum_of_squares.R
+++ R/TomLeversRPackage/R/calculate_residual_sum_of_squares.R
@@ -1,5 +1,6 @@
 calculate_residual_sum_of_squares <- function(linear_model) {
     residuals <- linear_model$residuals
     residual_sum_of_squares <- t(residuals) %*% residuals
+    residual_sum_of_squares <- residual_sum_of_squares[1, 1]
     return(residual_sum_of_squares)
 }
