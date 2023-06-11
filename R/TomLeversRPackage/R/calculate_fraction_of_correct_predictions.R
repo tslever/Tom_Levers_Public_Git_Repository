f3e85d061db57b6fdb08457333ac854856aab261
Tom Lever <thomas.lever.business@gmail.com>
2023-06-10 23:05:30 -0600

    Allowed calculating fraction of correct predictions for QDA models. Experimented with QDA models.

--- R/TomLeversRPackage/R/calculate_fraction_of_correct_predictions.R
+++ R/TomLeversRPackage/R/calculate_fraction_of_correct_predictions.R
@@ -18,10 +18,12 @@ calculate_fraction_of_correct_predictions <- function(model, data_set) {
   condition <- vector_of_predicted_probabilities > 0.5
   vector_of_predicted_directions[condition] = "Up"
   name_of_response <- names(model$model)[1]
- } else if ("lda" %in% vector_of_classes_of_model) {
+ } else if (("lda" %in% vector_of_classes_of_model) | "qda" %in% vector_of_classes_of_model) {
   prediction <- predict(model, newdata = data_set)
   vector_of_predicted_directions <- prediction$class
   name_of_response <- names(attr(model$terms, "dataClasses"))[1]
+ } else {
+  stop("A fractions of correct predictions may not be yet calculated for models of the class of parameter model.")
  }
  confusion_matrix <- table(
   vector_of_predicted_directions,
