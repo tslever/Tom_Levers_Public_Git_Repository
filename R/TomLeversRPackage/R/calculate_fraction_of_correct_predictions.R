d76ac94b373d132ab745c390a20d63b5ed6de42f
Tom Lever <thomas.lever.business@gmail.com>
2023-06-11 12:01:00 -0600

    Modified calculate_fraction_of_correct_predictions to build and predict with models

--- R/TomLeversRPackage/R/calculate_fraction_of_correct_predictions.R
+++ R/TomLeversRPackage/R/calculate_fraction_of_correct_predictions.R
@@ -5,29 +5,45 @@
 #' @examples fraction_of_correct_predictions <- calculate_fraction_of_correct_predictions(model = Direction_vs_Lag2, data_set = Weekly_from_2009_to_2010_inclusive)
 
 #' @export
-calculate_fraction_of_correct_predictions <- function(model, data_set) {
- vector_of_classes_of_model <- class(model)
- number_of_observations <- nrow(data_set)
- if ("glm" %in% vector_of_classes_of_model) {
+calculate_fraction_of_correct_predictions <- function(type_of_model, formula, training_data, test_data) {
+ number_of_test_observations <- nrow(test_data)
+ if (type_of_model == "LR") {
+  LR_model <- glm(
+   formula = formula,
+   data = training_data,
+   family = binomial
+  )
   vector_of_predicted_probabilities <- predict(
-   object = model,
-   newdata = data_set,
+   object = LR_model,
+   newdata = test_data,
    type = "response"
   )
-  vector_of_predicted_directions <- rep("Down", number_of_observations)
+  vector_of_predicted_directions <- rep("Down", number_of_test_observations)
   condition <- vector_of_predicted_probabilities > 0.5
   vector_of_predicted_directions[condition] = "Up"
-  name_of_response <- names(model$model)[1]
- } else if (("lda" %in% vector_of_classes_of_model) | "qda" %in% vector_of_classes_of_model) {
-  prediction <- predict(model, newdata = data_set)
+  name_of_response <- names(LR_model$model)[1]
+ } else if (type_of_model == "LDA") {
+  LDA_model <- lda(
+   formula = formula,
+   data = training_data
+  )
+  prediction <- predict(LDA_model, newdata = test_data)
   vector_of_predicted_directions <- prediction$class
-  name_of_response <- names(attr(model$terms, "dataClasses"))[1]
+  name_of_response <- names(attr(LDA_model$terms, "dataClasses"))[1]
+ } else if (type_of_model == "QDA") {
+  QDA_model <- qda(
+   formula = formula,
+   data = training_data
+  )
+  prediction <- predict(QDA_model, newdata = test_data)
+  vector_of_predicted_directions <- prediction$class
+  name_of_response <- names(attr(QDA_model$terms, "dataClasses"))[1]
  } else {
-  stop("A fractions of correct predictions may not be yet calculated for models of the class of parameter model.")
+  stop("A fraction of correct predictions may not be yet calculated for models of the class of parameter model.")
  }
  confusion_matrix <- table(
   vector_of_predicted_directions,
-  data_set[, name_of_response]
+  test_data[, name_of_response]
  )
  number_of_true_negatives <- confusion_matrix[1, 1]
  number_of_false_negatives <- confusion_matrix[1, 2]
