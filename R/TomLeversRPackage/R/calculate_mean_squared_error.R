ec6e870961bc7a4acf92103c4cd234c6b62c6ddf
Tom Lever <thomas.lever.business@gmail.com>
2023-06-15 17:16:39 -0600

    Added calculate_mean_squared_error to TomLeversRPackage

---
+++ R/TomLeversRPackage/R/calculate_mean_squared_error.R
@@ -0,0 +1,15 @@
+#' @title calculate_mean_squared_error
+#' @description Calculates mean squared error
+#' @param model The model
+#' @return mean_squared_error The mean squared error
+#' @examples mean_squared_error <- calculate_mean_squared_error(model)
+
+#' @export
+calculate_mean_squared_error <- function(model) {
+ vector_of_residuals <- model$residuals
+ vector_of_squared_residuals <- vector_of_residuals^2
+ sum_of_squared_residuals <- sum(vector_of_squared_residuals)
+ number_of_residuals <- length(vector_of_residuals)
+ mean_squared_error <- sum_of_squared_residuals / number_of_residuals
+ return(mean_squared_error)
+}
