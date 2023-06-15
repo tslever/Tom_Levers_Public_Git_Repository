cdf60b9b583c963e44bf69efac4cb05eb63f178e
Tom Lever <thomas.lever.business@gmail.com>
2023-06-15 17:17:22 -0600

    Allowed calculating LOOCV error rates for polynomial regression models

--- R/TomLeversRPackage/R/calculate_LOOCV_error_rate.R
+++ R/TomLeversRPackage/R/calculate_LOOCV_error_rate.R
@@ -6,8 +6,22 @@
 
 #' @export
 calculate_LOOCV_error_rate <- function(type_of_model, formula, data_frame) {
- if (type_of_model == "Logistic Regression") {
-  number_of_observations <- nrow(data_frame)
+ number_of_observations <- nrow(data_frame)
+ if (type_of_model == "Polynomial Regression") {
+  vector_of_mean_squared_errors <- double(0)
+  for (i in 1:number_of_observations) {
+   training_data <- data_frame[-i, ]
+   testing_data <- data_frame[i, ]
+   linear_regression_model <- glm(
+    formula = formula,
+    data = training_data
+   )
+   mean_squared_error <- calculate_mean_squared_error(linear_regression_model)
+   vector_of_mean_squared_errors <- append(vector_of_mean_squared_errors, mean_squared_error)
+  }
+  LOOCV_error_rate <- mean(vector_of_mean_squared_errors)
+  return(LOOCV_error_rate)
+ } else if (type_of_model == "Logistic Regression") {
   vector_of_indicators_of_error <- integer(0)
   for (i in 1:number_of_observations) {
    training_data <- data_frame[-i, ]
