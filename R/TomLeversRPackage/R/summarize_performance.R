83823b2813eb82ea93b4beec141fbd72fbcf0f03
Tom Lever <thomas.lever.business@gmail.com>
2023-06-16 21:00:48 -0700

    Added Polynomial Regression to summarize_performance. Decoupled calculation of numbers of FN, FP, TN, and TP from confusion matrix. Added calculation of rate of false positives. Decoupled TPR and equation in summary of performance.

--- R/TomLeversRPackage/R/summarize_performance.R
+++ R/TomLeversRPackage/R/summarize_performance.R
@@ -1,6 +1,6 @@
 #' @title summarize_performance
 #' @description Generates a summary of performance for a model given a data set for which to predict
-#' @param type_of_model The type of model. An element in the set {"Logistic Regression", "LDA", "QDA", "KNN"}.
+#' @param type_of_model The type of model. An element in the set {"Polynomial Regression", "Logistic Regression", "LDA", "QDA", "KNN"}.
 #' @param formula The formula for the model
 #' @param training_data A data frame of training data
 #' @param test_data A data frame of test data
@@ -12,7 +12,14 @@
 #' @export
 summarize_performance <- function(type_of_model, formula, training_data, test_data, K = 1) {
  number_of_test_observations <- nrow(test_data)
- if (type_of_model == "Logistic Regression") {
+ if (type_of_model == "Polynomial Regression") {
+  linear_regression_model <- glm(
+   formula = formula,
+   data = training_data
+  )
+  mean_squared_error <- calculate_mean_squared_error(linear_regression_model)
+  return(mean_squared_error)
+ } else if (type_of_model == "Logistic Regression") {
   LR_model <- glm(
    formula = formula,
    data = training_data,
@@ -69,30 +76,45 @@ summarize_performance <- function(type_of_model, formula, training_data, test_da
   test_data[, name_of_response]
  )
  map_of_binary_value_to_response_value <- contrasts(x = training_data[, name_of_response])
- number_of_true_negatives <- confusion_matrix[1, 1]
- number_of_false_negatives <- confusion_matrix[1, 2]
- number_of_false_positives <- confusion_matrix[2, 1]
- number_of_true_positives <- confusion_matrix[2, 2]
+ number_of_false_negatives <- 0
+ number_of_false_positives <- 0
+ number_of_true_negatives <- 0
+ number_of_true_positives <- 0
+ for (j in 1:number_of_test_observations) {
+  if (vector_of_predicted_response_values[j] == 0 & test_data[j, name_of_response] == 1) {
+      number_of_false_negatives <- number_of_false_negatives + 1
+  } else if (vector_of_predicted_response_values[j] == 1 & test_data[j, name_of_response] == 0) {
+      number_of_false_positives <- number_of_false_positives + 1
+  } else if (vector_of_predicted_response_values[j] == 0 & test_data[j, name_of_response] == 0) {
+      number_of_true_negatives <- number_of_true_negatives + 1
+  } else if (vector_of_predicted_response_values[j] == 1 & test_data[j, name_of_response] == 1) {
+      number_of_true_positives <- number_of_true_positives + 1
+  }
+ }
  number_of_correct_predictions <-
   number_of_true_negatives + number_of_true_positives
  decimal_of_correct_predictions <-
   number_of_correct_predictions / number_of_test_observations
- error_rate <- 1 - decimal_of_correct_predictions
+ rate_of_errors <- 1 - decimal_of_correct_predictions
  fraction_of_correct_predictions <- paste(number_of_correct_predictions, " / ", number_of_test_observations, sep = "")
+ rate_of_false_positives <- number_of_false_positives / (number_of_false_positives + number_of_true_negatives)
  rate_of_true_positives <- number_of_true_positives / (number_of_true_positives + number_of_false_negatives)
  rate_of_true_negatives <- number_of_true_negatives / (number_of_true_negatives + number_of_false_positives)
  summary_of_performance <- list(
-     confusion_matrix = confusion_matrix,
-     decimal_of_correct_predictions = decimal_of_correct_predictions,
-     error_rate = error_rate,
-     fraction_of_correct_predictions = fraction_of_correct_predictions,
-     map_of_binary_value_to_response_value = map_of_binary_value_to_response_value,
-     equation_for_number_of_true_negatives = paste("TN = CM[1, 1] = ", number_of_true_negatives, sep = ""),
-     equation_for_number_of_false_negatives = paste("FN = CM[1, 2] = ", number_of_false_negatives, sep = ""),
-     equation_for_number_of_false_positives = paste("FP = CM[2, 1] = ", number_of_false_positives, sep = ""),
-     equation_for_number_of_true_positives = paste("TP = CM[2, 2] = ", number_of_true_positives, sep = ""),
-     equation_for_true_positive_rate = paste("TPR = Sensitivity = Recall = Hit Rate = TP/P = TP/(TP+FN) = ", rate_of_true_positives, sep = ""),
-     equation_for_true_negative_rate = paste("TNR = Specificity = Selectivity = TN/N = TN/(TN+FP) = ", rate_of_true_negatives, sep = "")
+  confusion_matrix = confusion_matrix,
+  decimal_of_correct_predictions = decimal_of_correct_predictions,
+  rate_of_errors = rate_of_errors,
+  fraction_of_correct_predictions = fraction_of_correct_predictions,
+  map_of_binary_value_to_response_value = map_of_binary_value_to_response_value,
+  equation_for_number_of_true_negatives = paste("TN = CM[1, 1] = ", number_of_true_negatives, sep = ""),
+  equation_for_number_of_false_negatives = paste("FN = CM[1, 2] = ", number_of_false_negatives, sep = ""),
+  equation_for_number_of_false_positives = paste("FP = CM[2, 1] = ", number_of_false_positives, sep = ""),
+  equation_for_number_of_true_positives = paste("TP = CM[2, 2] = ", number_of_true_positives, sep = ""),
+  equation_for_false_positive_rate = paste("FPR = Fall-Out = FP/N = FP/(FP+TN) = ", sep = ""),
+  rate_of_false_positives = rate_of_false_positives,
+  equation_for_true_positive_rate = paste("TPR = Sensitivity = Recall = Hit Rate = TP/P = TP/(TP+FN) = ", sep = ""),
+  rate_of_true_positives = rate_of_true_positives,
+  equation_for_true_negative_rate = paste("TNR = Specificity = Selectivity = TN/N = TN/(TN+FP) = ", rate_of_true_negatives, sep = "")
  )
  class(summary_of_performance) <- "summary_of_performance"
  return(summary_of_performance)
