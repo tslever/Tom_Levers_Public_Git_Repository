829855fb800dcf6dd8ed4bca9f9c912d157a4937
Tom Lever <thomas.lever.business@gmail.com>
2023-06-16 19:21:57 -0700

    Fix: Calculated error rate for each test fold. Generalized LOOCV to CV.

--- R/TomLeversRPackage/R/calculate_CV_error_rate.R
+++ R/TomLeversRPackage/R/calculate_CV_error_rate.R
@@ -1,8 +1,8 @@
-#' @title calculate_LOOCV_error_rate
-#' @description Calculates LOOCV error rate
-#' @param type_of_model The type of model. An element in the set {"Logistic Regression"}.
-#' @return LOOCV_error_rate The error rate of Leave One Out Cross Validation
-#' @examples LOOCV_error_rate <- calculate_LOOCV_error_rate(type_of_model = "LR", formula = Direction ~ Lag1 + Lag2, data_frame = ISLR2::Weekly)
+#' @title calculate_CV_error_rate
+#' @description Calculates CV error rate
+#' @param type_of_model The type of model. An element in the set {"Polynomial Regression", Logistic Regression"}.
+#' @return CV_error_rate The error rate of Leave One Out Cross Validation
+#' @examples CV_error_rate <- calculate_CV_error_rate(type_of_model = "LR", formula = Direction ~ Lag1 + Lag2, data_frame = ISLR2::Weekly)
 
 #' @export
 calculate_CV_error_rate <- function(type_of_model, formula, data_frame, number_of_folds) {
@@ -33,10 +33,10 @@ calculate_CV_error_rate <- function(type_of_model, formula, data_frame, number_o
    mean_squared_error <- calculate_mean_squared_error(linear_regression_model)
    vector_of_mean_squared_errors <- append(vector_of_mean_squared_errors, mean_squared_error)
   }
-  LOOCV_error_rate <- mean(vector_of_mean_squared_errors)
-  return(LOOCV_error_rate)
+  CV_error_rate <- mean(vector_of_mean_squared_errors)
+  return(CV_error_rate)
  } else if (type_of_model == "Logistic Regression") {
-  vector_of_indicators_of_error <- integer(0)
+  vector_of_error_rates <- integer(0)
   for (i in 1:number_of_folds) {
    if (number_of_folds == 1) {
     training_data <- data_frame
@@ -72,18 +72,19 @@ calculate_CV_error_rate <- function(type_of_model, formula, data_frame, number_o
    vector_of_predicted_response_values <- rep(lower_level, number_of_test_observations)
    condition <- vector_of_predicted_probabilities > 0.5
    vector_of_predicted_response_values[condition] <- upper_level
+   sum_of_indicators_of_error <- 0
    for (j in 1:number_of_test_observations) {
-    if (vector_of_predicted_response_values[j] == testing_data[j, name_of_response]) {
-     vector_of_indicators_of_error <- append(vector_of_indicators_of_error, 0)
-    } else {
-     vector_of_indicators_of_error <- append(vector_of_indicators_of_error, 1)
+    if (vector_of_predicted_response_values[j] != testing_data[j, name_of_response]) {
+     sum_of_indicators_of_error <- sum_of_indicators_of_error + 1
     }
    }
+   error_rate <- sum_of_indicators_of_error / number_of_test_observations
+   vector_of_error_rates <- append(vector_of_error_rates, error_rate)
   }
-  LOOCV_error_rate <- mean(vector_of_indicators_of_error)
-  return(LOOCV_error_rate)
+  CV_error_rate <- mean(vector_of_error_rates)
+  return(CV_error_rate)
  } else {
-  error_message <- paste("LOOCV error rate may not be calculated yet for type of model ", type_of_model, sep = "")
+  error_message <- paste("CV error rate may not be calculated yet for type of model ", type_of_model, sep = "")
   stop(error_message)
  }
 }
