ec9e1f129d5d4f930f5e35a415e3e5a79edc285f
Tom Lever <thomas.lever.business@gmail.com>
2023-06-22 02:25:23 -0400

    Transitioned provide_data_for_ROC_curve to provide_data_for_ROC_and_PR_curves

--- R/TomLeversRPackage/R/provide_data_for_ROC_and_PR_curves.R
+++ R/TomLeversRPackage/R/provide_data_for_ROC_and_PR_curves.R
@@ -8,19 +8,19 @@
 #' @import rsample
 
 #' @export
-provide_data_for_ROC_curve <- function(vector_of_actual_indicators, vector_of_predicted_probabilities) {
- sequence <- seq(from = 0, to = 1, by = 0.01)
- data_frame_of_thresholds_TPRs_and_FPRs <- setNames(
+provide_data_for_ROC_and_PR_curves <- function(vector_of_actual_indicators, vector_of_predicted_probabilities) {
+ sequence <- seq(from = 0, to = 1, by = 0.005)
+ data_frame_of_thresholds_TPRs_FPRs_and_PPVs <- setNames(
   object = data.frame(
    matrix(
-    ncol = 3,
+    ncol = 4,
     nrow = length(sequence)
-   ),
-   nm = c("threshold", "TPR", "FPR")
-  )
+   )
+  ),
+  nm = c("threshold", "TPR", "FPR", "PPV")
  )
- for (i in sequence) {
-  vector_of_predicted_indicators <- as.numeric(vector_of_predicted_probabilities > i)
+ for (i in 1:length(sequence)) {
+  vector_of_predicted_indicators <- as.numeric(vector_of_predicted_probabilities > sequence[i])
   number_of_false_negatives <- sum(vector_of_predicted_indicators == 0 & vector_of_actual_indicators == 1)
   number_of_false_positives <- sum(vector_of_predicted_indicators == 1 & vector_of_actual_indicators == 0)
   number_of_true_negatives <- sum(vector_of_predicted_indicators == 0 & vector_of_actual_indicators == 0)
@@ -29,9 +29,12 @@ provide_data_for_ROC_curve <- function(vector_of_actual_indicators, vector_of_pr
   rate_of_true_positives <- number_of_true_positives / (number_of_true_positives + number_of_false_negatives)
   # FPR = Fall-Out = FP/N = FP/(FP+TN)
   rate_of_false_positives <- number_of_false_positives / (number_of_false_positives + number_of_true_negatives)
-  data_frame_of_thresholds_TPRs_and_FPRs[i, 1] <- i
-  data_frame_of_thresholds_TPRs_and_FPRs[i, 2] <- rate_of_true_positives
-  data_frame_of_thresholds_TPRs_and_FPRs[i, 3] <- rate_of_false_positives
+  # PPV = Precision = Positive Predictive Value = TP/\hat{P} = TP/(TP+FP)
+  precision <- number_of_true_positives / (number_of_true_positives + number_of_false_positives)
+  data_frame_of_thresholds_TPRs_FPRs_and_PPVs[i, 1] <- sequence[i]
+  data_frame_of_thresholds_TPRs_FPRs_and_PPVs[i, 2] <- rate_of_true_positives
+  data_frame_of_thresholds_TPRs_FPRs_and_PPVs[i, 3] <- rate_of_false_positives
+  data_frame_of_thresholds_TPRs_FPRs_and_PPVs[i, 4] <- precision
  }
- return(data_frame_of_thresholds_TPRs_and_FPRs)
+ return(data_frame_of_thresholds_TPRs_FPRs_and_PPVs)
 }
