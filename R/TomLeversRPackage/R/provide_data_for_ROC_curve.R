50b3b3c0ba0b5d06f72744a9990f106b960422db
Tom Lever <thomas.lever.business@gmail.com>
2023-06-22 01:14:15 -0400

    Preinitialized data frame to be returned by provide_data_for_ROC_curve

--- R/TomLeversRPackage/R/provide_data_for_ROC_curve.R
+++ R/TomLeversRPackage/R/provide_data_for_ROC_curve.R
@@ -9,10 +9,17 @@
 
 #' @export
 provide_data_for_ROC_curve <- function(vector_of_actual_indicators, vector_of_predicted_probabilities) {
- vector_of_thresholds <- double(0)
- vector_of_rates_of_true_positives <- double(0)
- vector_of_rates_of_false_positives <- double(0)
- for (i in seq(from = 0, to = 1, by = 0.01)) {
+ sequence <- seq(from = 0, to = 1, by = 0.01)
+ data_frame_of_thresholds_TPRs_and_FPRs <- setNames(
+  object = data.frame(
+   matrix(
+    ncol = 3,
+    nrow = length(sequence)
+   ),
+   nm = c("threshold", "TPR", "FPR")
+  )
+ )
+ for (i in sequence) {
   vector_of_predicted_indicators <- as.numeric(vector_of_predicted_probabilities > i)
   number_of_false_negatives <- sum(vector_of_predicted_indicators == 0 & vector_of_actual_indicators == 1)
   number_of_false_positives <- sum(vector_of_predicted_indicators == 1 & vector_of_actual_indicators == 0)
@@ -22,14 +29,9 @@ provide_data_for_ROC_curve <- function(vector_of_actual_indicators, vector_of_pr
   rate_of_true_positives <- number_of_true_positives / (number_of_true_positives + number_of_false_negatives)
   # FPR = Fall-Out = FP/N = FP/(FP+TN)
   rate_of_false_positives <- number_of_false_positives / (number_of_false_positives + number_of_true_negatives)
-  vector_of_thresholds <- append(vector_of_thresholds, i)
-  vector_of_rates_of_true_positives <- append(vector_of_rates_of_true_positives, rate_of_true_positives)
-  vector_of_rates_of_false_positives <- append(vector_of_rates_of_false_positives, rate_of_false_positives)
+  data_frame_of_thresholds_TPRs_and_FPRs[i, 1] <- i
+  data_frame_of_thresholds_TPRs_and_FPRs[i, 2] <- rate_of_true_positives
+  data_frame_of_thresholds_TPRs_and_FPRs[i, 3] <- rate_of_false_positives
  }
- data_frame_of_thresholds_TPRs_and_FPRs <- data.frame(
-  threshold = vector_of_thresholds,
-  TPR = vector_of_rates_of_true_positives,
-  FPR = vector_of_rates_of_false_positives
- )
  return(data_frame_of_thresholds_TPRs_and_FPRs)
 }
