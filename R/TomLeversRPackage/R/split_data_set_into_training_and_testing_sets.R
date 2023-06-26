f27cbcf194bedc7cbb39dabca97d5cfaa0b4d469
Tom Lever <thomas.lever.business@gmail.com>
2023-06-26 02:13:54 -0400

    Added vector_of_indices_of_training_data and vector_of_indices_of_testing_data to output of split_data_set_into_training_and_testing_data

--- R/TomLeversRPackage/R/split_data_set_into_training_and_testing_sets.R
+++ R/TomLeversRPackage/R/split_data_set_into_training_and_testing_sets.R
@@ -9,11 +9,17 @@
 split_data_set_into_training_and_testing_data <- function(data_frame, proportion_of_training_data) {
  number_of_observations <- nrow(data_frame)
  vector_of_random_indices <- sample(1:number_of_observations)
- shuffled_data_frame <- data_frame[vector_of_random_indices, ]
  number_of_training_data <- ceiling(number_of_observations * proportion_of_training_data)
- training_data <- shuffled_data_frame[1:number_of_training_data, ]
- testing_data <- shuffled_data_frame[(number_of_training_data + 1) : number_of_observations, ]
- split_data <- list(training_data = training_data, testing_data = testing_data)
+ vector_of_indices_of_training_data <- vector_of_random_indices[1:number_of_training_data]
+ vector_of_indices_of_testing_data <- vector_of_random_indices[(number_of_training_data + 1) : number_of_observations]
+ training_data <- data_frame[vector_of_indices_of_training_data, ]
+ testing_data <- data_frame[vector_of_indices_of_testing_data, ]
+ split_data <- list(
+  vector_of_indices_of_training_data = vector_of_indices_of_training_data,
+  vector_of_indices_of_testing_data = vector_of_indices_of_testing_data,
+  training_data = training_data,
+  testing_data = testing_data
+ )
  class(split_data) <- "split_data"
  return(split_data)
 }
