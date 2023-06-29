ad25d34b9d07b8871c834957ee9d3f95fd31a0bf
Tom Lever <thomas.lever.business@gmail.com>
2023-06-29 03:28:50 -0400

    Added accuracy to provide_performance_metrics

--- R/TomLeversRPackage/R/provide_performance_metrics.R
+++ R/TomLeversRPackage/R/provide_performance_metrics.R
@@ -13,11 +13,11 @@ provide_performance_metrics <- function(vector_of_actual_indicators, vector_of_p
  data_frame_of_performance_metrics <- setNames(
   object = data.frame(
    matrix(
-    ncol = 5,
+    ncol = 6,
     nrow = length(sequence)
    )
   ),
-  nm = c("threshold", "TPR", "FPR", "PPV", "decimal_of_true_positives")
+  nm = c("accuracy", "decimal_of_true_positives", "FPR", "PPV", "threshold", "TPR")
  )
  number_of_observations <- length(vector_of_actual_indicators)
  for (i in 1:length(sequence)) {
@@ -26,6 +26,8 @@ provide_performance_metrics <- function(vector_of_actual_indicators, vector_of_p
   number_of_false_positives <- sum(vector_of_predicted_indicators == 1 & vector_of_actual_indicators == 0)
   number_of_true_negatives <- sum(vector_of_predicted_indicators == 0 & vector_of_actual_indicators == 0)
   number_of_true_positives <- sum(vector_of_predicted_indicators == 1 & vector_of_actual_indicators == 1)
+  number_of_true_predictions <- number_of_true_negatives + number_of_true_positives
+  accuracy <- number_of_true_predictions / number_of_observations
   # TPR = Sensitivity = Recall = Hit Rate = TP/P = TP/(TP+FN)
   rate_of_true_positives <- number_of_true_positives / (number_of_true_positives + number_of_false_negatives)
   # FPR = Fall-Out = FP/N = FP/(FP+TN)
@@ -33,13 +35,14 @@ provide_performance_metrics <- function(vector_of_actual_indicators, vector_of_p
   # PPV = Precision = Positive Predictive Value = TP/\hat{P} = TP/(TP+FP)
   number_of_positive_predictions <- number_of_true_positives + number_of_false_positives
   precision <- number_of_true_positives / number_of_positive_predictions
-  #decimal_of_predicted_positives <- number_of_positive_predictions / number_of_observations
   decimal_of_true_positives <- number_of_true_positives / number_of_observations
-  data_frame_of_performance_metrics[i, 1] <- sequence[i]
-  data_frame_of_performance_metrics[i, 2] <- rate_of_true_positives
-  data_frame_of_performance_metrics[i, 3] <- rate_of_false_positives
-  data_frame_of_performance_metrics[i, 4] <- precision
-  data_frame_of_performance_metrics[i, 5] <- decimal_of_true_positives
+  data_frame_of_performance_metrics[i, "accuracy"] <- accuracy
+  data_frame_of_performance_metrics[i, "decimal_of_true_positives"] <- decimal_of_true_positives
+  data_frame_of_performance_metrics[i, "FPR"] <- rate_of_false_positives
+  data_frame_of_performance_metrics[i, "PPV"] <- precision
+  data_frame_of_performance_metrics[i, "threshold"] <- sequence[i]
+  data_frame_of_performance_metrics[i, "TPR"] <- rate_of_true_positives
+
  }
  return(data_frame_of_performance_metrics)
 }
