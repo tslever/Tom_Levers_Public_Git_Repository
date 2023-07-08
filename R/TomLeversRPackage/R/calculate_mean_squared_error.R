73a49e57bb015984d31e46ee775e379cdf2752cf
Tom Lever <thomas.lever.business@gmail.com>
2023-07-08 15:23:49 -0400

    Rewrote calculate_mean_squared_error to receive vector_of_predicted_response_values and vector_of_actual_response_values

--- R/TomLeversRPackage/R/calculate_mean_squared_error.R
+++ R/TomLeversRPackage/R/calculate_mean_squared_error.R
@@ -5,8 +5,9 @@
 #' @examples mean_squared_error <- calculate_mean_squared_error(model)
 
 #' @export
-calculate_mean_squared_error <- function(model) {
- vector_of_residuals <- model$residuals
+calculate_mean_squared_error <- function(vector_of_predicted_response_values, vector_of_actual_response_values) {
+ #vector_of_residuals <- model$residuals
+ vector_of_residuals <- vector_of_predicted_response_values - vector_of_actual_response_values
  vector_of_squared_residuals <- vector_of_residuals^2
  sum_of_squared_residuals <- sum(vector_of_squared_residuals)
  number_of_residuals <- length(vector_of_residuals)
