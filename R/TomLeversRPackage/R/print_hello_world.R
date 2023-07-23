4c09b0b518fded7b30f500ef5aa5875eb0cb966e
Tom Lever <thomas.lever.business@gmail.com>
2023-07-23 13:11:36 -0400

    Added print_hello_world

--- R/TomLeversRPackage/R/print_hello_world.R
+++ R/TomLeversRPackage/R/print_hello_world.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,7 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
+#' @export
+print_hello_world <- function() {
   print("Hello, world!")
 }
