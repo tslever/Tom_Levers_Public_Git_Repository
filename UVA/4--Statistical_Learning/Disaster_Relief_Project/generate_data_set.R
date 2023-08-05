e669cc004cd50360868c218e8bdbeea24228f809
Tom Lever <thomas.lever.business@gmail.com>
2023-08-05 01:41:28 -0400

    Generated holdout data frame

--- UVA/4--Statistical_Learning/Disaster_Relief_Project/generate_data_set.R
+++ UVA/4--Statistical_Learning/Disaster_Relief_Project/generate_data_set.R
@@ -1,6 +1,6 @@
 generate_data_set <- function(vector_of_paths_to_data_files) {
  number_of_paths_to_data_files <- length(vector_of_paths_to_data_files)
- header <- c("ID", "X", "Y", "Map X", "Map Y", "Lat", "Lon", "B1", "B2", "B3")
+ header <- c("ID", "X", "Y", "Map X", "Map Y", "Lat", "Lon", "B1", "B2", "B3", "Class")
  composite_data_frame <- data.frame(matrix(ncol = length(header), nrow = 0))
  colnames(composite_data_frame) <- header
  vector_of_numbers_of_rows <- rep(0, number_of_paths_to_data_files)
@@ -9,6 +9,14 @@ generate_data_set <- function(vector_of_paths_to_data_files) {
    file = vector_of_paths_to_data_files[i],
    comment.char = ";"
   )
+  number_of_rows <- nrow(data_frame)
+  vector_of_classes <- rep("", number_of_rows)
+  if (grepl("NON", vector_of_paths_to_data_files[i], fixed = TRUE) | grepl("NOT", vector_of_paths_to_data_files[i], fixed = TRUE)) {
+   vector_of_classes <- rep("Not Blue Tarp", number_of_rows)
+  } else {
+   vector_of_classes <- rep("Blue Tarp", number_of_rows)
+  }
+  data_frame$Class <- vector_of_classes
   colnames(data_frame) <- header
   composite_data_frame <- rbind(composite_data_frame, data_frame)
   vector_of_numbers_of_rows[i] = nrow(data_frame)
---
+++ UVA/4--Statistical_Learning/Disaster_Relief_Project/generate_holdout_data_frame.R
@@ -0,0 +1,21 @@
+vector_of_paths_to_data_files <- c(
+ "C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/Holdout_Data/orthovnir057_ROI_NON_Blue_Tarps.txt",
+ "C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/Holdout_Data/orthovnir067_ROI_Blue_Tarps.txt",
+ "C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/Holdout_Data/orthovnir067_ROI_NOT_Blue_Tarps.txt",
+ "C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/Holdout_Data/orthovnir069_ROI_Blue_Tarps.txt",
+ "C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/Holdout_Data/orthovnir069_ROI_NOT_Blue_Tarps.txt",
+ "C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/Holdout_Data/orthovnir078_ROI_Blue_Tarps.txt",
+ "C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/Holdout_Data/orthovnir078_ROI_NON_Blue_Tarps.txt"
+)
+source("C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/generate_data_set.R")
+holdout_data_set <- generate_data_set(vector_of_paths_to_data_files)
+print(head(holdout_data_set$data_frame))
+print(nrow(holdout_data_set$data_frame))
+print(holdout_data_set$vector_of_numbers_of_rows)
+holdout_data_frame <- holdout_data_set$data_frame[, c("Class", "B1", "B2", "B3")]
+colnames(holdout_data_frame) <- c("Class", "Red", "Green", "Blue")
+print(head(holdout_data_frame))
+print(nrow(holdout_data_frame))
+write.csv(holdout_data_set, "C:/Users/Tom/Documents/Tom_Levers_Public_Git_Repository/UVA/4--Statistical_Learning/Disaster_Relief_Project/Holdout_Data/Holdout_Data_Frame.csv", row.names = FALSE)
+print(nrow(holdout_data_frame[holdout_data_frame$Class == "Not Blue Tarp",]))
+print(nrow(holdout_data_frame[holdout_data_frame$Class == "Blue Tarp",]))
