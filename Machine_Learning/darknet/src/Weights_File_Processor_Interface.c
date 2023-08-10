7f6a20899ae46302e9450e2c16bc7f9f6e6388ef
Tom Lever <thomas.lever.business@gmail.com>
2023-08-10 16:40:04 -0400

    Removed darknet

--- Machine_Learning/darknet/src/Weights_File_Processor_Interface.c
+++
@@ -1,9 +0,0 @@
-#include "darknet.h"
-#include "Weights_File_Processor_Interface.h"
-
-
-void DNLIB_load_weights(network* net, char* filename) {
-
-    load_weights(net, filename);
-
-}
--- Machine_Learning/darknet/src/Weights_File_Processor_Interface_For_Testing.c
+++
@@ -1,9 +0,0 @@
-#include "darknet.h"
-#include "Weights_File_Processor_Interface_For_Testing.h"
-
-
-void DNLIB_load_weights_for_testing(network* net, char* filename) {
-
-    printf("Testing DNLIB_load_weights_for_testing.\n");
-
-}
