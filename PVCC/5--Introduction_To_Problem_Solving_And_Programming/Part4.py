c55c96dd42e6bb8075e987413c5d03d9b3bfbd07
Tom Lever <thomas.lever.business@gmail.com>
2023-12-08 14:57:02 -0500

    Completed Part 4

--- PVCC/5--Introduction_To_Problem_Solving_And_Programming/Part4.py
+++ PVCC/5--Introduction_To_Problem_Solving_And_Programming/Part4.py
@@ -10,26 +10,41 @@
 
 import os
 
+# Source: ChatGPT 3.5
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            is_upper = char.isupper()
+            char = char.lower()
+            shifted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            if is_upper:
+                shifted_char = shifted_char.upper()
+            result += shifted_char
+        else:
+            result += char
+    return result
+
 plaintext_or_cyphertext = input('Do you wish to enter plaintext to convert to cyphertext or decode existing cyphertext (plaintext / cyphertext)? ')
 if plaintext_or_cyphertext == 'cyphertext':
     if os.path.exists('cypher.txt'):
         with open('cypher.txt', 'r') as file:
             cyphertext = file.read()
-            print(f'I read cyphertext {cyphertext}.')
-            distance = input('Enter integer distance: ')
+            print(f'I read cyphertext "{cyphertext}".')
+            string_representing_distance = input('Enter integer distance: ')
+            distance = int(string_representing_distance)
             print(f'You entered distance {distance}.')
-            # TODO: Decode cyphertext into plaintext
-            plaintext = 'placeholder plaintext'
+            plaintext = caesar_cipher(cyphertext, -distance)
             print(f'Your cyphertext is decoded into plaintext "{plaintext}".')
     else:
-        raise FileExistsError('cypher.txt does not exist: Use this program to convert plaintext into cyphertext.')
+        print('cypher.txt does not exist: Use this program to convert plaintext into cyphertext.')
 elif plaintext_or_cyphertext == 'plaintext':
     plaintext = input('Enter any printable character plaintext: ')
     print(f'You entered plaintext "{plaintext}".')
-    distance = input('Enter integer distance: ')
+    string_representing_distance = input('Enter integer distance: ')
+    distance = int(string_representing_distance)
     print(f'You entered distance {distance}.')
-    # TODO: Encode plaintext into cyphertext
-    cyphertext = 'placeholder cyphertext'
+    cyphertext = caesar_cipher(plaintext, distance)
     print(f'Your plaintext is encoded into cyphertext "{cyphertext}".')
     with open('cypher.txt', 'w') as file:
         file.write(cyphertext)
