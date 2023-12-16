900ce51e0eee8aab6bfe3beabc800dd1ee09034c
Tom Lever <thomas.lever.business@gmail.com>
2023-12-16 16:59:10 -0500

    Update getlistofanchortagswithhyperlinkreferences.py

--- Python/tomleverspythonpackage/tomleverspythonpackage/getlistofanchortagswithhyperlinkreferences.py
+++ Python/tomleverspythonpackage/tomleverspythonpackage/getlistofanchortagswithhyperlinkreferences.py
@@ -1,11 +1,14 @@
+# Gets list of anchor tags with hyperlink references given a URL
+# Usage:
+# python .\getlistofanchortagswithhyperlinkreferences.py https://www.google.com
+# from tomleverspythonpackage.getlistofanchortagswithhyperlinkreferences import get_list_of_anchor_tags_with_hyperlink_references; get_list_of_anchor_tags_with_hyperlink_references('https://www.google.com')
+
+import argparse
 from bs4 import BeautifulSoup
 import requests
-import sys
 
-def get_list_of_anchor_tags_with_hyperlink_references(URL, username, password):
+def get_list_of_anchor_tags_with_hyperlink_references(URL):
     session = requests.Session()
-    session.auth = (username, password)
-    auth = session.post('https://www.doi.org')
     response = session.get(URL)
     text = response.text
     beautiful_soup = BeautifulSoup(text, 'html')
@@ -13,8 +16,16 @@ def get_list_of_anchor_tags_with_hyperlink_references(URL, username, password):
     for anchor_tag in list_of_anchor_tags:
         print(anchor_tag['href'])
 
+def parse_arguments():
+    dictionary_of_arguments = {}
+    parser = argparse.ArgumentParser(prog = 'Get List Of Anchor Tags With Hyperlink References', description = 'This program gets a list of anchor tags with hyperlink references.')
+    parser.add_argument('URL', help = 'URL')
+    args = parser.parse_args()
+    URL = args.URL
+    print(f'URL: {URL}')
+    dictionary_of_arguments['URL'] = URL
+    return dictionary_of_arguments
+
 if __name__ == "__main__":
-    URL = sys.argv[1]
-    username = sys.argv[2]
-    password = sys.argv[3]
-    get_list_of_anchor_tags_with_hyperlink_references(URL, username, password)
+    dictionary_of_arguments = parse_arguments()
+    get_list_of_anchor_tags_with_hyperlink_references(dictionary_of_arguments['URL'])
