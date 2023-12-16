d741879304a136129869fac12ff9ebb38ae29abd
Tom Lever <thomas.lever.business@gmail.com>
2023-12-16 17:25:28 -0500

    Update upgradepippackages.py

--- Python/tomleverspythonpackage/tomleverspythonpackage/upgradepippackages.py
+++ Python/tomleverspythonpackage/tomleverspythonpackage/upgradepippackages.py
@@ -1,3 +1,8 @@
+# Upgrades pip packages
+# Usage:
+# python upgradepippackages.py
+# from tomleverspythonpackage.upgradepippackages import upgrade_pip_packages; upgrade_pip_packages()
+
 import subprocess
 
 def upgrade_pip_packages():
@@ -7,3 +12,6 @@ def upgrade_pip_packages():
             line = line.split('==')
             line = line[0]
             subprocess.run(f'pip install --upgrade {line}', shell = True)
+
+if __name__ == '__main__':
+    upgrade_pip_packages()
