3ce700e1b3cf33bd7f0beacc5cc74414705cdbc7
Tom Lever <thomas.lever.business@gmail.com>
2024-01-01 14:47:03 -0500

    Fix: The effect of the mana ability of a forest is to add a green mana.

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_Forest.java
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_Forest.java
@@ -2,9 +2,10 @@ package Com.TSL.The_Utilities_For_The_MTG_Game_Simulator;
 
 public class a_Forest extends a_land {
 
-	public a_Forest(boolean The_Tapped_Status) {
-		super("Forest", The_Tapped_Status);
-		a_mana_ability The_Mana_Ability = new a_mana_ability("T", "Add [F].", this);
+	public a_Forest() {
+		super("Forest");
+		a_mana_ability The_Mana_Ability = new a_mana_ability("T", "Add [G].", this);
+		
 		this.receives(The_Mana_Ability);
 	}
 }
