ba04f008c7ffe90421673d5b89515708ec234902
Tom Lever <thomas.lever.business@gmail.com>
2024-01-05 00:46:34 -0500

    Neatened a_land

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_land.java
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_land.java
@@ -1,7 +1,5 @@
 package Com.TSL.The_Utilities_For_The_MTG_Game_Simulator;
 
-import java.util.ArrayList;
-
 public class a_land extends a_permanent {
 	
 	public a_land(String The_Name, a_player The_Player) {
