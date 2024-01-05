a3e17df25a403576818a399409a39f44ec566676
Tom Lever <thomas.lever.business@gmail.com>
2024-01-05 00:25:14 -0500

    Neatened a_graveyard

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_graveyard.java
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_graveyard.java
@@ -1,11 +1,8 @@
 package Com.TSL.The_Utilities_For_The_MTG_Game_Simulator;
 
-
 import java.util.ArrayList;
 
-
-public class a_graveyard
-{
+public class a_graveyard {
 
 	private ArrayList<a_card> List_Of_Cards;
 	
@@ -16,5 +13,4 @@ public class a_graveyard
 	public void receives(a_card The_Card) {
 		this.List_Of_Cards.add(The_Card);
 	}
-	
 }
