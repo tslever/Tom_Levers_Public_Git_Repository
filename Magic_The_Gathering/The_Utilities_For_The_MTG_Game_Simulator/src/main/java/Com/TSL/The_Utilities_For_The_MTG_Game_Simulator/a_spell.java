723a6f962ae57f10c0ab7b9326cd020138113704
Tom Lever <thomas.lever.business@gmail.com>
2024-01-06 12:20:47 -0500

    Neatened a_spell

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_spell.java
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_spell.java
@@ -2,38 +2,39 @@ package Com.TSL.The_Utilities_For_The_MTG_Game_Simulator;
 
 
 /**
+ * a_spell
+ * 
  * Rule 405.4: Each spell has all the characteristics of the card associated with it... The controller of a spell is the person who cast it.
- *
  */
 public class a_spell {
 
+	private boolean Indicator_Of_Whether_This_Spell_Has_A_Target;
 	private String Name;
-	private boolean Indicator_Of_Whether_This_Spell_Has_A_Target = false;
+	private a_nonland_card Nonland_Card;
 	private a_player Player;
 	private String Type;
-	private a_nonland_card Nonland_Card;
 	
 	public a_spell(String The_Name_To_Use, a_nonland_card The_Nonland_Card, a_player The_Player_To_Use, String The_Type_To_Use) {
-		this.Player = The_Player_To_Use;
 		this.Name = The_Name_To_Use;
-		this.Type = The_Type_To_Use;
 		this.Nonland_Card = The_Nonland_Card;
+		this.Player = The_Player_To_Use;
+		this.Type = The_Type_To_Use;
 	}
 	
 	public boolean has_a_target() {
 		return this.Indicator_Of_Whether_This_Spell_Has_A_Target;
 	}
 	
-	public a_player player() {
-		return this.Player;
-	}
-	
 	public String name() {
 		return this.Name;
 	}
 	
-	public String type() {
-		return this.Type;
+	public a_nonland_card nonland_card() {
+		return this.Nonland_Card;
+	}
+	
+	public a_player player() {
+		return this.Player;
 	}
 	
 	@Override
@@ -41,7 +42,7 @@ public class a_spell {
 		return this.Name;
 	}
 	
-	public a_nonland_card nonland_card() {
-		return this.Nonland_Card;
+	public String type() {
+		return this.Type;
 	}
 }
