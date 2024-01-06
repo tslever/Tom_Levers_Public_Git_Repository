38ff8d87334212938f7212a619689a79f223dec6
Tom Lever <thomas.lever.business@gmail.com>
2024-01-06 12:17:02 -0500

    Neatened a_planeswalker

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_planeswalker.java
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/a_planeswalker.java
@@ -1,13 +1,12 @@
 package Com.TSL.The_Utilities_For_The_MTG_Game_Simulator;
 
-
 public class a_planeswalker extends a_permanent {
 	
-	private int loyalty;
 	private int Combat_Damage;
+	private int Loyalty;
 	
-	public a_planeswalker(String The_Name, boolean The_Tapped_Status, a_player The_Player) {
-		super(The_Name, The_Player);
+	public a_planeswalker(String The_Name_To_Use, a_player The_Player_To_Use) {
+		super(The_Name_To_Use, The_Player_To_Use);
 	}
 	
 	public void receives_combat_damage(int The_Combat_Damage) {
