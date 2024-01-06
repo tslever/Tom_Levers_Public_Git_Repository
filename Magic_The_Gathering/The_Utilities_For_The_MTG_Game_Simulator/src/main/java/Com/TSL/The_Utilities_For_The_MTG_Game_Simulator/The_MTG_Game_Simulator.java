a603ce326b871e1cf36cdf057d474c6beb744c18
Tom Lever <thomas.lever.business@gmail.com>
2024-01-06 12:43:47 -0500

    Neatened The_MTG_Game_Simulator

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/The_MTG_Game_Simulator.java
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/src/main/java/Com/TSL/The_Utilities_For_The_MTG_Game_Simulator/The_MTG_Game_Simulator.java
@@ -9,13 +9,9 @@ public class The_MTG_Game_Simulator {
     	a_deck_history The_Deck_History_For_Keep_the_Peace = new a_deck_history(The_Deck_Keep_The_Peace, 0, 0);
     	System.out.println(The_Deck_History_For_Keep_the_Peace);
     	a_deck The_Deck_Large_And_In_Charge = The_Deck_Builder.builds_Large_And_In_Charge();
-    	System.out.println(
-    		"The deck Large and in Charge has " + The_Deck_Large_And_In_Charge.number_of_cards() + " cards and is the following. " + The_Deck_Large_And_In_Charge
-    	);
-    	
+    	System.out.println("The deck Large and in Charge has " + The_Deck_Large_And_In_Charge.number_of_cards() + " cards and is the following. " + The_Deck_Large_And_In_Charge);
     	a_deck_history The_Deck_History_For_Large_and_in_Charge = new a_deck_history(The_Deck_Large_And_In_Charge, 0, 0);
     	System.out.println(The_Deck_History_For_Large_and_in_Charge);
-    	
     	a_stack The_Stack = new a_stack();
     	a_player The_First_Player = new a_player(The_Deck_Keep_The_Peace, "Tom", The_Stack);
     	a_player The_Second_Player = new a_player(The_Deck_Large_And_In_Charge, "Scott", The_Stack);
