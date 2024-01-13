7e3f5d5192365ae1037161a4a7737905e8c787fc
Tom Lever <thomas.lever.business@gmail.com>
2024-01-13 01:35:14 -0500

    On button click, table updates with the first row being the text of the response

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/App.tsx
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/App.tsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 import ButtonDisplayer from "./ButtonDisplayer";
 import Displayer from "./Displayer";
 import TableDisplayer from "./TableDisplayer";
 
 function App() {
-  const bodyData : String[][] = [
-    ["Line 1"],
-    ["Line 2"]
-  ]
+  const [bodyData, setBodyData] = useState<string[][]>([]);
+  const onClick = async () => {
+    try {
+      const response = await fetch('http://localhost:8080');
+      const text = await response.text();
+      const rowWithText = [text];
+      const newBodyData = [];
+      newBodyData.push(rowWithText);
+      for (let i = 0; i < bodyData.length; i++) {
+        newBodyData.push(bodyData[i]);
+      }
+      setBodyData(newBodyData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <Displayer>
-      <ButtonDisplayer label = "Next"/>
+      <ButtonDisplayer
+        label = "Next"
+        onClick = { onClick }
+      />
       <TableDisplayer
         headerData = { ["Action"] }
         bodyData = { bodyData }
--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/ButtonDisplayer.tsx
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/ButtonDisplayer.tsx
@@ -1,25 +1,16 @@
 import Displayer from './Displayer';
 
 type Props = {
-  label: string;
+  label: String;
+  onClick: () => void;
 };
 
 function ButtonDisplayer(props: Props): JSX.Element {
-  const handleButtonClick = async () => {
-    try {
-      const response = await fetch('http://localhost:8080');
-      const data = await response.text();
 
-      // Handle the data or do something with it
-      console.log(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
 
   return (
     <Displayer>
-      <button onClick = { handleButtonClick }>
+      <button onClick = { props.onClick }>
         { props.label }
       </button>
     </Displayer>
