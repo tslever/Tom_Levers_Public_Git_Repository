470439d2394ebfb4ec2a15d0ea156a93bdd1d324
Tom Lever <thomas.lever.business@gmail.com>
2024-01-13 01:48:10 -0500

    Used both Displayers and Caretakers

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/App.tsx
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/App.tsx
@@ -5,32 +5,40 @@ import Displayer from "./Displayer";
 import TableDisplayer from "./TableDisplayer";
 
 function App() {
-  const [bodyData, setBodyData] = useState<string[][]>([]);
-  const onClick = async () => {
-    try {
-      const response = await fetch('http://localhost:8080');
-      const text = await response.text();
-      const rowWithText = [text];
-      const newBodyData = [];
-      newBodyData.push(rowWithText);
-      for (let i = 0; i < bodyData.length; i++) {
-        newBodyData.push(bodyData[i]);
+  
+  function Caretaker() {
+    const [bodyData, setBodyData] = useState<string[][]>([]);
+    const onClick = async () => {
+      try {
+        const response = await fetch('http://localhost:8080');
+        const text = await response.text();
+        const rowWithText = [text];
+        const newBodyData = [];
+        newBodyData.push(rowWithText);
+        for (let i = 0; i < bodyData.length; i++) {
+          newBodyData.push(bodyData[i]);
+        }
+        setBodyData(newBodyData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-      setBodyData(newBodyData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
+    };
+    return {
+      "bodyData": bodyData,
+      "onClick": onClick
     }
-  };
-
+  }
+  const jsonObjectWithBodyDataAndOnClick = Caretaker();
+  
   return (
     <Displayer>
       <ButtonDisplayer
         label = "Next"
-        onClick = { onClick }
+        onClick = { jsonObjectWithBodyDataAndOnClick.onClick }
       />
       <TableDisplayer
         headerData = { ["Action"] }
-        bodyData = { bodyData }
+        bodyData = { jsonObjectWithBodyDataAndOnClick.bodyData }
       />
     </Displayer>
   );
