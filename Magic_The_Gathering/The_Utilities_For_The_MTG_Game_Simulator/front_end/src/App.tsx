67fc995702ac1abd8aed89e6afe0bba2137fe639
Tom Lever <thomas.lever.business@gmail.com>
2024-01-13 00:27:10 -0500

    Added body data to table

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/App.tsx
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/App.tsx
@@ -1,15 +1,19 @@
-import { useEffect } from 'react';
 import './App.css';
 import ButtonDisplayer from "./ButtonDisplayer";
 import Displayer from "./Displayer";
 import TableDisplayer from "./TableDisplayer";
 
 function App() {
+  const bodyData : String[][] = [
+    ["Line 1"],
+    ["Line 2"]
+  ]
   return (
     <Displayer>
       <ButtonDisplayer label = "Next"/>
       <TableDisplayer
         headerData = { ["Action"] }
+        bodyData = { bodyData }
       />
     </Displayer>
   );
--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/TableDisplayer.tsx
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/TableDisplayer.tsx
@@ -2,6 +2,7 @@ import Displayer from "./Displayer";
 
 type Props = {
     headerData?: String[]
+    bodyData: JSX.Element[][] | String[][]
 };
 
 function TableDisplayer (props: Props): JSX.Element {
@@ -23,10 +24,30 @@ function TableDisplayer (props: Props): JSX.Element {
             </thead>
         );
     }
+    const bodyRows: JSX.Element[] = [];
+    for (let i = 0; i < (props.bodyData).length; i++) {
+        const dataRow = (props.bodyData)[i];
+        const bodyRow: JSX.Element[] = [];
+        for (let j = 0; j < dataRow.length; j++) {
+            bodyRow.push(
+                <td key = { j }>
+                    { dataRow[j] }
+                </td>
+            );
+        }
+        bodyRows.push(
+            <tr key = { i }>
+                { bodyRow }
+            </tr>
+        );
+    }
     return (
         <Displayer>
             <table>
                 { header }
+                <tbody>
+                    { bodyRows }
+                </tbody>
             </table>
         </Displayer>
     );
