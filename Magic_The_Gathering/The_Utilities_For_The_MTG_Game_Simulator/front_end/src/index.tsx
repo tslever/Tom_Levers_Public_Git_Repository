ecc2459dc4b78dac3fe51e10693576a5ccd31010
Tom Lever <thomas.lever.business@gmail.com>
2024-01-13 13:11:01 -0500

    Brought front end in line with CODES

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/index.tsx
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/index.tsx
@@ -1,3 +1,14 @@
+/**
+ * A Shell organizes one or more Organizers into a payload to be delivered to a client.
+ * A Shell provides the rules and mechanisms for how Organizers are made available to a user.
+ * A Shell provides no functionality other than that required to implement those rules and mechanisms.
+ * A Shell manages how one or more Organizers are made visible to a user (e.g., simultaneously or by user selection).
+ * A Shell is a React component.
+ * A Shell is the top-level React component of a payload.
+ * The artifact of building a Shell is a single HTML file to be delivered to a client when it makes a request to an endpoint (e.g., http://localhost:3000).
+ * A Shell with two or more Organizers has a background color and specifies characteristics for each HTML division that bounds an Organizer.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
