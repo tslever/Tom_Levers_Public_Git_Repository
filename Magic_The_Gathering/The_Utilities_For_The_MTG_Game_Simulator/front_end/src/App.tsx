7ce16e5063f7981c95a54ffb58b07efe11452939
Tom Lever <thomas.lever.business@gmail.com>
2024-01-13 00:12:59 -0500

    Response is logged when button is clicked

--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/App.tsx
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/App.tsx
@@ -5,22 +5,6 @@ import Displayer from "./Displayer";
 import TableDisplayer from "./TableDisplayer";
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/echo');
-        const data = await response.text();
-
-        // Handle the data or do something with it
-        console.log(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []); // Empty dependency array ensures the effect runs once when the component mounts
-
   return (
     <Displayer>
       <ButtonDisplayer label = "Next"/>
--- Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/ButtonDisplayer.tsx
+++ Magic_The_Gathering/The_Utilities_For_The_MTG_Game_Simulator/front_end/src/ButtonDisplayer.tsx
@@ -1,17 +1,29 @@
-import Displayer from "./Displayer";
+import Displayer from './Displayer';
 
 type Props = {
-    label: String
+  label: string;
 };
 
-function TableDisplayer (props: Props): JSX.Element {
-    return (
-        <Displayer>
-            <button>
-                { props.label }
-            </button>
-        </Displayer>
-    );
-};
+function ButtonDisplayer(props: Props): JSX.Element {
+  const handleButtonClick = async () => {
+    try {
+      const response = await fetch('http://localhost:8080');
+      const data = await response.text();
 
-export default TableDisplayer;
+      // Handle the data or do something with it
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <Displayer>
+      <button onClick = { handleButtonClick }>
+        { props.label }
+      </button>
+    </Displayer>
+  );
+}
+
+export default ButtonDisplayer;
