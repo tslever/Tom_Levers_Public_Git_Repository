e9789720eab79490652d9ba48b6c4872168887bf
Tom Lever <thomas.lever.business@gmail.com>
2024-01-21 01:06:44 -0500

    Created front end for Settlers Of Catan

---
+++ Machine_Learning/Settles_Of_Catan/front_end/src/TableDisplayer.tsx
@@ -0,0 +1,58 @@
+import Desert from "./Desert.png";
+import Displayer from "./Displayer";
+
+type Props = {
+    headerData?: String[]
+    bodyData: JSX.Element[][] | String[][]
+};
+
+function TableDisplayer (props: Props): JSX.Element {
+    const headerCells: JSX.Element[] = [];
+    let header: JSX.Element = <></>;
+    if ((props.headerData !== null) && (props.headerData !== undefined)) {
+        for (let i = 0; i < (props.headerData).length; i++) {
+            headerCells.push(
+                <th key = { i }>
+                    { (props.headerData)[i] }
+                </th>
+            );
+        }
+        header = (
+            <thead>
+                <tr>
+                    { headerCells }
+                </tr>
+            </thead>
+        );
+    }
+    const bodyRows: JSX.Element[] = [];
+    for (let i = 0; i < (props.bodyData).length; i++) {
+        const dataRow = (props.bodyData)[i];
+        const bodyRow: JSX.Element[] = [];
+        for (let j = 0; j < dataRow.length; j++) {
+            bodyRow.push(
+                <td key = { j }>
+                    { dataRow[j] }
+                </td>
+            );
+        }
+        bodyRows.push(
+            <tr key = { i }>
+                { bodyRow }
+            </tr>
+        );
+    }
+    return (
+        <Displayer>
+            <img src = {Desert}/>
+            <table>
+                { header }
+                <tbody>
+                    { bodyRows }
+                </tbody>
+            </table>
+        </Displayer>
+    );
+};
+
+export default TableDisplayer;
