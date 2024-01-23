6e5a10f07c850a134803ac1e2709e80b265bdb6a
Tom Lever <thomas.lever.business@gmail.com>
2024-01-23 02:35:55 -0500

    Constrained isometric grid to size of Catan base board. Began to express positions in terms of isometric x and y coordinates. Neatened code for generating grid lines.

--- Machine_Learning/Settles_Of_Catan/front_end/src/CanvasDisplayer.tsx
+++ Machine_Learning/Settles_Of_Catan/front_end/src/CanvasDisplayer.tsx
@@ -8,17 +8,18 @@ type Props = {
   widthPercentage: number
 };
 
-const width_of_scroll_bar_in_Google_Chrome_for_Windows_10 = 17
-const width_of_webpage = window.innerWidth - width_of_scroll_bar_in_Google_Chrome_for_Windows_10
+function get_canvas_x_coordinate_given(width_of_canvas: number, isometric_x_coordinate: number) {
+  return width_of_canvas / 2 + isometric_x_coordinate * width_of_canvas / 20;
+}
 
 var drawGrid = function(ctx: CanvasRenderingContext2D, w: number, h: number, step: number) {
   const tangent_of_30_degrees = 0.5773502691896257645091487805019574556476017512701268760186023264;
   
   // Vertical Lines
   ctx.beginPath(); 
-  for (var x = 0; x <= w; x += step / 2) {
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, h);
+  for (var x = -10; x <= 10; x += 1) {
+    ctx.moveTo(get_canvas_x_coordinate_given(w, x), 0);
+    ctx.lineTo(get_canvas_x_coordinate_given(w, x), h);
   }
   ctx.strokeStyle = 'rgb(0,0,0)';
   ctx.lineWidth = 1;
@@ -30,9 +31,11 @@ var drawGrid = function(ctx: CanvasRenderingContext2D, w: number, h: number, ste
     ctx.moveTo(x, 0);
     ctx.lineTo(0, tangent_of_30_degrees * x);
   }
-  ctx.strokeStyle = 'rgb(0,0,0)';
+  ctx.moveTo(w, 0);
+  ctx.lineTo(0, tangent_of_30_degrees * w);
+  ctx.strokeStyle = 'rgb(255,0,0)';
   ctx.lineWidth = 1;
-  ctx.stroke(); 
+  ctx.stroke();
   
   // Negatively Sloping Lines Beginning On Top Edge
   ctx.beginPath(); 
@@ -40,7 +43,7 @@ var drawGrid = function(ctx: CanvasRenderingContext2D, w: number, h: number, ste
     ctx.moveTo(x, 0);
     ctx.lineTo(w, tangent_of_30_degrees * (w - x));
   }
-  ctx.strokeStyle = 'rgb(0,0,0)';
+  ctx.strokeStyle = 'rgb(0,255,0)';
   ctx.lineWidth = 1;
   ctx.stroke();
   
@@ -50,7 +53,7 @@ var drawGrid = function(ctx: CanvasRenderingContext2D, w: number, h: number, ste
     ctx.moveTo(0, y);
     ctx.lineTo(w, y + w * tangent_of_30_degrees);
   }
-  ctx.strokeStyle = 'rgb(0,0,0)';
+  ctx.strokeStyle = 'rgb(0,0,255)';
   ctx.lineWidth = 1;
   ctx.stroke();
 
@@ -60,49 +63,49 @@ var drawGrid = function(ctx: CanvasRenderingContext2D, w: number, h: number, ste
     ctx.moveTo(w, y);
     ctx.lineTo(0, y + w * tangent_of_30_degrees);
   }
-  ctx.strokeStyle = 'rgb(0,0,0)';
+  ctx.strokeStyle = 'rgb(255,0,255)';
   ctx.lineWidth = 1;
   ctx.stroke();
 
   // Tile 0: Desert
   let region = new Path2D();
-  region.moveTo(6 * step, 6 * step * tangent_of_30_degrees);
-  region.lineTo(7 * step, 7 * step * tangent_of_30_degrees);
-  region.lineTo(7 * step, 9 * step * tangent_of_30_degrees);
-  region.lineTo(6 * step, 6 * step * tangent_of_30_degrees);
+  region.moveTo(5 * step, 6 * step * tangent_of_30_degrees);
+  region.lineTo(6 * step, 7 * step * tangent_of_30_degrees);
+  region.lineTo(6 * step, 9 * step * tangent_of_30_degrees);
+  region.lineTo(5 * step, 6 * step * tangent_of_30_degrees);
   region.closePath();
   ctx.fillStyle = '#F5D5A1';
   ctx.fill(region);
 
   // Tile 4: Brick
   region = new Path2D();
-  region.moveTo(5 * step, 3 * step * tangent_of_30_degrees);
-  region.lineTo(6 * step, 4 * step * tangent_of_30_degrees);
-  region.lineTo(6 * step, 6 * step * tangent_of_30_degrees);
-  region.lineTo(5 * step, 3 * step * tangent_of_30_degrees);
+  region.moveTo(4 * step, 3 * step * tangent_of_30_degrees);
+  region.lineTo(5 * step, 4 * step * tangent_of_30_degrees);
+  region.lineTo(5 * step, 6 * step * tangent_of_30_degrees);
+  region.lineTo(4 * step, 3 * step * tangent_of_30_degrees);
   region.closePath();
   ctx.fillStyle = '#AA4A44';
   ctx.fill(region);
 
   // Tile 14: Ore
   region = new Path2D();
-  region.moveTo(4 * step, 0);
-  region.lineTo(5 * step, 1 * step * tangent_of_30_degrees);
-  region.lineTo(5 * step, 3 * step * tangent_of_30_degrees);
-  region.lineTo(4 * step, 4 * step * tangent_of_30_degrees);
-  region.lineTo(3 * step, 3 * step * tangent_of_30_degrees);
-  region.lineTo(3 * step, 1 * step * tangent_of_30_degrees);
-  region.lineTo(4 * step, 0);
+  region.moveTo(3 * step, 0);
+  region.lineTo(4 * step, 1 * step * tangent_of_30_degrees);
+  region.lineTo(4 * step, 3 * step * tangent_of_30_degrees);
+  region.lineTo(3 * step, 4 * step * tangent_of_30_degrees);
+  region.lineTo(2 * step, 3 * step * tangent_of_30_degrees);
+  region.lineTo(2 * step, 1 * step * tangent_of_30_degrees);
+  region.lineTo(3 * step, 0);
   region.closePath();
   ctx.fillStyle = '#52595D';
   ctx.fill(region);
 
   // Tile 18: Ore
   region = new Path2D();
-  region.moveTo(10 * step, 6 * step * tangent_of_30_degrees);
-  region.lineTo(11 * step, 7 * step * tangent_of_30_degrees);
-  region.lineTo(11 * step, 9 * step * tangent_of_30_degrees);
-  region.lineTo(10 * step, 6 * step * tangent_of_30_degrees);
+  region.moveTo(9 * step, 6 * step * tangent_of_30_degrees);
+  region.lineTo(10 * step, 7 * step * tangent_of_30_degrees);
+  region.lineTo(10 * step, 9 * step * tangent_of_30_degrees);
+  region.lineTo(9 * step, 6 * step * tangent_of_30_degrees);
   region.closePath();
   ctx.fillStyle = '#52595D';
   ctx.fill(region);
@@ -116,12 +119,17 @@ function CanvasDisplayer(props: Props) {
     if (canvas) {
       const context = canvas.getContext('2d');
       if (context) {
-        drawGrid(context, canvas.width, canvas.height, canvas.width / 12);
+        drawGrid(context, canvas.width, canvas.height, canvas.width / 10);
       }
     }
   }, []);
+  //const width_of_scroll_bar_in_Google_Chrome_for_Windows_10 = 17
+  //const width_of_webpage = window.innerWidth - width_of_scroll_bar_in_Google_Chrome_for_Windows_10
+  const height_of_webpage = window.innerHeight;
   return (
-    <canvas ref = { canvasRef } width = { width_of_webpage } height = { width_of_webpage } style = { { backgroundColor: '#ffffff' } }/>
+    <center>
+      <canvas ref = { canvasRef } width = { height_of_webpage - 4 } height = { height_of_webpage - 4 } style = { { backgroundColor: '#ffffff' } }/>
+    </center>
   );
 }
 
