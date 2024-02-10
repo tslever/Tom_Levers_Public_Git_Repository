9ad9ed3bd8ffa6ab247eea233f78fc697fd8700f
Tom Lever <thomas.lever.business@gmail.com>
2024-02-10 14:06:41 -0500

    Back end provides list of possilbe actions. Modularized displayers. listOfPossibleActions is updated instead of list of action displayers.

--- Machine_Learning/Settlers_Of_Catan/front_end/src/Front_End.tsx
+++ Machine_Learning/Settlers_Of_Catan/front_end/src/Front_End.tsx
@@ -1,15 +1,11 @@
 import ActionDisplayer from './ActionDisplayer';
-import BaseBoardDisplayer from './BaseBoardDisplayer'
 import Displayer from './Displayer';
 import { useState } from 'react';
-import TableDisplayer from './TableDisplayer';
-import TableDisplayerOfActivePlayersHandCards from './TableDisplayerOfActivePlayersHandCards';
-import TableDisplayerOfBankCards from './TableDisplayerOfBankCards';
-import TableDisplayerOfNonactivePlayersHandCards from './TableDisplayerOfNonactivePlayersHandCards';
-import TableDisplayerOfMessages from './TableDisplayerOfMessages';
-import TableDisplayerOfMenuOfActions from './TableDisplayerOfMenuOfActions';
+import PrimaryTableDisplayer from './PrimaryTableDisplayer';
 
 function Front_End() {
+
+  const [listOfPossibleActions, setListOfPossibleActions] = useState(['Click me to get started.'])
 
   async function act(action: string) {
     const url = 'http://localhost:5000';
@@ -23,59 +19,23 @@ function Front_End() {
     });  
     const json = await response.json();
     console.log(json);
-    const displayer_of_action = <ActionDisplayer
-      backgroundColor='#ffffff'
-      act = { act }
-    >
-      { json.next_action }
-    </ActionDisplayer>
-    setListOfActions([displayer_of_action]);
+    setListOfPossibleActions(json.list_of_possible_actions);
   }
 
-  const displayer_of_action_click_me_to_get_started = <ActionDisplayer
-    backgroundColor = '#ffffff'
-    act = { act }
-  >
-    Click me to get started.
-  </ActionDisplayer>
-  const [listOfActionDisplayers, setListOfActions] = useState([displayer_of_action_click_me_to_get_started]);
+  function mapToActionDisplayer(possibleAction: string) {
+    return <ActionDisplayer
+      act = { act }
+      backgroundColor = 'white'
+    >
+      { possibleAction }
+    </ActionDisplayer>
+  }
 
+  const listOfActionDisplayers = listOfPossibleActions.map(mapToActionDisplayer);
 
-  const column_group_for_table_for_base_board_displayer_and_menu_of_actions = <colgroup>
-    <col style = { { width: '50%' } }/>
-    <col style = { { width: '50%' } }/>
-  </colgroup>
-  const table_displayer_for_base_board_displayer_and_menu_of_actions = <TableDisplayer
-    bodyData = { [[<BaseBoardDisplayer/>, <TableDisplayerOfMenuOfActions listOfActionDisplayers = {listOfActionDisplayers}/>]] }
-    colgroup = { column_group_for_table_for_base_board_displayer_and_menu_of_actions }
-    widthPercentage = { 100 }
-  />
-
-  const body_data_for_primary_table_displayer = [
-    [table_displayer_for_base_board_displayer_and_menu_of_actions],
-    [<TableDisplayerOfBankCards/>],
-    [<TableDisplayerOfActivePlayersHandCards/>],
-    [<TableDisplayerOfNonactivePlayersHandCards/>],
-    [<TableDisplayerOfMessages/>]
-  ];
-  const column_group_for_primary_table = <colgroup>
-    <col style = { { width: '100%' } }/>
-  </colgroup>
-  const row_styles_for_primary_table = [
-    { 'backgroundColor': 'rgb(255, 248, 195)' },
-    { 'backgroundColor': 'rgb(255, 243, 137)' },
-    { 'backgroundColor': 'rgb(255, 248, 195)' },
-    { 'backgroundColor': 'rgb(255, 243, 137)' }
-  ]
-  const primary_table_displayer = <TableDisplayer
-    bodyData = { body_data_for_primary_table_displayer }
-    colgroup = { column_group_for_primary_table }
-    rowStyles = { row_styles_for_primary_table }
-    widthPercentage = { 100 }
-  />
   return (
     <Displayer backgroundColor = 'rgb(255, 248, 195)'>
-      { primary_table_displayer }
+      { <PrimaryTableDisplayer listOfActionDisplayers = { listOfActionDisplayers }/> }
     </Displayer>
   );
 }
---
+++ Machine_Learning/Settlers_Of_Catan/front_end/src/TableDisplayerForBaseBoardDisplayerAndMenuOfActions.tsx
@@ -0,0 +1,21 @@
+import BaseBoardDisplayer from "./BaseBoardDisplayer"
+import TableDisplayer from "./TableDisplayer"
+import TableDisplayerOfMenuOfActions from "./TableDisplayerOfMenuOfActions"
+
+type Props = {
+    listOfActionDisplayers: JSX.Element[]
+}
+
+function TableDisplayerForBaseBoardDisplayerAndMenuOfActions(props: Props) {
+  const column_group_for_table_for_base_board_displayer_and_menu_of_actions = <colgroup>
+    <col style = { { width: '50%' } }/>
+    <col style = { { width: '50%' } }/>
+  </colgroup>
+  return <TableDisplayer
+    bodyData = { [[<BaseBoardDisplayer/>, <TableDisplayerOfMenuOfActions listOfActionDisplayers = {props.listOfActionDisplayers}/>]] }
+    colgroup = { column_group_for_table_for_base_board_displayer_and_menu_of_actions }
+    widthPercentage = { 100 }
+  />
+}
+
+export default TableDisplayerForBaseBoardDisplayerAndMenuOfActions;
